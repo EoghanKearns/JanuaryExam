15e87662ea608657588476c663e829d8b294c77c
Eoghan Kearns <s00221854@mail.itsligo.ie>
2023-01-05 11:11:55 +0000

    finished q1 g - created players and added them to teams

--- Q1/MainWindow.xaml.cs
+++ Q1/MainWindow.xaml.cs
@@ -50,6 +50,47 @@ namespace Q1
             // displays in list box
             lbxTeams.ItemsSource = teams;
 
+            // create players
+           List<Player> players = new List<Player>();
+
+            //French players
+            Player p1 = new Player() { Name = "Marie", ResultRecord = "WWDDL" };
+            Player p2 = new Player() { Name = "Claude", ResultRecord = "DDDLW" };
+            Player p3 = new Player() { Name = "Antoine", ResultRecord = "LWDLW" };
+
+            //Italian players
+            Player p4 = new Player() { Name = "Marco", ResultRecord = "WWDLL" };
+            Player p5 = new Player() { Name = "Giovanni", ResultRecord = "LLLLD" };
+            Player p6 = new Player() { Name = "Valentina", ResultRecord = "DLWWW" };
+
+            //Spanish players
+            Player p7 = new Player() { Name = "Maria", ResultRecord = "WWWWW" };
+            Player p8 = new Player() { Name = "Jose", ResultRecord = "LLLLL" };
+            Player p9 = new Player() { Name = "Pablo", ResultRecord = "DDDDD" };
+
+            // add players to players list
+            players.Add(p1);
+            players.Add(p2);
+            players.Add(p3);
+            players.Add(p4);
+            players.Add(p5);
+            players.Add(p6);
+            players.Add(p7);
+            players.Add(p8);
+            players.Add(p9);
+
+            // add players to teams
+            t1.Players.Add(p1);
+            t1.Players.Add(p2);
+            t1.Players.Add(p3);
+            t2.Players.Add(p4);
+            t2.Players.Add(p5);
+            t2.Players.Add(p6);
+            t3.Players.Add(p7);
+            t3.Players.Add(p8);
+            t3.Players.Add(p9);
+
+
             return teams;
         }
     }
